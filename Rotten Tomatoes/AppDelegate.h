e47d0f9b7f19fde83686ff9443e6110ad5f2db94
afzal273 <syafzal@gmail.com>
2015-02-03 22:23:22 -0800

    Initial Commit

---
+++ Rotten Tomatoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rotten Tomatoes
+//
+//  Created by Syed, Afzal on 2/3/15.
+//  Copyright (c) 2015 afzalsyed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Rotten Tomatoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rotten Tomatoes
+//
+//  Created by Syed, Afzal on 2/3/15.
+//  Copyright (c) 2015 afzalsyed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
